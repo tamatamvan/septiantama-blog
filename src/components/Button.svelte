52e90468b9e7b4bd9e2bff76793613853eeb343c
tamatamvan <sfirst.tama@gmail.com>
2019-10-07 00:17:11 +0700

    create Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+export let onClick
+export let text
+</script>
+
+<style>
+.btn {
+  border: none;
+  background: #5bbee5;
+  color: #fff;
+  font-size: 1.2em;
+  padding: .6em 1.2em;
+  cursor: pointer;
+}
+.btn:hover, .btn:active {
+  background: #0096d1;
+}
+</style>
+
+<button type="button" class="btn" on:click={onClick}>
+  {text}
+</button>
