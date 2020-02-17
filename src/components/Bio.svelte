8af1aa7658269e269389d9a26f1ea8c3400c7548
tamatamvan <sfirst.tama@gmail.com>
2020-02-18 04:45:30 +0700

    update bio

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -4,15 +4,6 @@
     display: flex;
   }
 
-  img {
-    width: 100px;
-    height: 100px;
-    border-radius: 100px;
-    /* border: 3px solid #5bbee5; */
-    display: block;
-    margin-right: 20px;
-  }
-
   p {
     font-size: 1.125rem;
   }
@@ -20,8 +11,7 @@
 
 <div>
   <p>
-    Hi, I'm <strong>Tama</strong>. I'm just an ordinary human being that currently working as a Web Developer
-    at a company called <a href="https://www.quipper.com/" target="_blank" rel="noreferer">Quipper.</a>
+    Hi, I'm <strong>Tama</strong>. I'm just an ordinary human being that currently working as a Web Developer.
     Beside tech, I'm also interested in many other things like education, literature, and economy.
   </p>
 </div>
